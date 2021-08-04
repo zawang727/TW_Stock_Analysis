8752363e002f68b4b829101aa8a3330b1e0c7d85
zawang727 <44350375+zawang727@users.noreply.github.com>
2021-08-04 11:06:57 +0800

    Add files via upload

--- stock_tw_analysis.py
+++ stock_tw_analysis.py
@@ -11,17 +11,123 @@ IOModuleFile = "stock_tw_io"
 IOModule = importlib.import_module(IOModuleFile)
 
 def five_days_MA_generation_for_a_stock(a_stock_info):
-    five_days_list = list()
-    for i in range (0,len(a_stock_info.history)):
-        five_days_list.append(a_stock_info.history[i].endprice)
-        if (len(five_days_list)>5): del five_days_list[0]
-        a_stock_info.analysis.append(IOModule.a_day_stock_analysis());
-        a_stock_info.analysis[i]=sum(five_days_list)/float(len(five_days_list))
+    days = 5
+    days_list = list()
+    for i in range (len(a_stock_info.history)-1,0,-1): #days loop
+        days_list.append(a_stock_info.history[i].endprice)
+        if (len(days_list)>days): del days_list[0]
+        a_stock_info.analysis[i].five_days_MA=sum(days_list)/float(len(days_list))
     
-
-stockdata_in_days = IOModule.fetch_all_stock_data()
-stockdata_in_stock_struct = IOModule.transfer_day_struct_2_stock_stock(stockdata_in_days)
-for i in stockdata_in_stock_struct.keys():
-    five_days_MA_generation_for_a_stock(stockdata_in_stock_struct[i])
+def ten_days_MA_generation_for_a_stock(a_stock_info):
+    days = 10
+    days_list = list()
+    for i in range (len(a_stock_info.history)-1,0,-1): #days loop
+        days_list.append(a_stock_info.history[i].endprice)
+        if (len(days_list)>days): del days_list[0]
+        a_stock_info.analysis[i].ten_days_MA=sum(days_list)/float(len(days_list))
+        
+def twnty_days_MA_generation_for_a_stock(a_stock_info):
+    days = 20
+    days_list = list()
+    for i in range (len(a_stock_info.history)-1,0,-1): #days loop
+        days_list.append(a_stock_info.history[i].endprice)
+        if (len(days_list)>days): del days_list[0]
+        a_stock_info.analysis[i].twnty_days_MA=sum(days_list)/float(len(days_list))
+        
+def sixty_days_MA_generation_for_a_stock(a_stock_info):
+    days = 60
+    days_list = list()
+    for i in range (len(a_stock_info.history)-1,0,-1): #days loop
+        days_list.append(a_stock_info.history[i].endprice)
+        if (len(days_list)>days): del days_list[0]
+        a_stock_info.analysis[i].sixty_days_MA=sum(days_list)/float(len(days_list))
+        
+def twoh_fifty_MA_generation_for_a_stock(a_stock_info):
+    days = 250
+    days_list = list()
+    for i in range (len(a_stock_info.history)-1,0,-1): #days loop
+        days_list.append(a_stock_info.history[i].endprice)
+        if (len(days_list)>days): del days_list[0]
+        a_stock_info.analysis[i].twoh_fifty_days_MA=sum(days_list)/float(len(days_list))
+        
+def twnty_days_max_generation(a_stock_info):
+    days = 20
+    maxprice = 0.0;
+    days_list = list()
+    for i in range (len(a_stock_info.history)-1,0,-1): #days loop
+        if (a_stock_info.history[i].maxprice>maxprice): 
+             days_list.append(a_stock_info.history[i].maxprice)
+             if (len(days_list)>days): del days_list[0]
+             a_stock_info.analysis[i].twnty_days_max=max(days_list)
+             
+def twnty_days_min_generation(a_stock_info):
+    days = 20
+    minprice = 100000.0;
+    days_list = list()
+    for i in range (len(a_stock_info.history)-1,0,-1): #days loop
+        if (a_stock_info.history[i].minprice<minprice): 
+             days_list.append(a_stock_info.history[i].minprice)
+             if (len(days_list)>days): del days_list[0]
+             a_stock_info.analysis[i].twnty_days_min=min(days_list)
+             
+def sixty_days_max_generation(a_stock_info):
+    days = 60
+    maxprice = 0.0;
+    days_list = list()
+    for i in range (len(a_stock_info.history)-1,0,-1): #days loop
+        if (a_stock_info.history[i].maxprice>maxprice): 
+             days_list.append(a_stock_info.history[i].maxprice)
+             if (len(days_list)>days): del days_list[0]
+             a_stock_info.analysis[i].sixty_days_max=max(days_list)
+             
+def sixty_days_min_generation(a_stock_info):
+    days = 60
+    minprice = 100000.0;
+    days_list = list()
+    for i in range (len(a_stock_info.history)-1,0,-1): #days loop
+        if (a_stock_info.history[i].minprice<minprice): 
+             days_list.append(a_stock_info.history[i].minprice)
+             if (len(days_list)>days): del days_list[0]
+             a_stock_info.analysis[i].sixty_days_min=min(days_list)
+             
+def twoh_fifty_days_max_generation(a_stock_info):
+    days = 250
+    maxprice = 0.0;
+    days_list = list()
+    for i in range (len(a_stock_info.history)-1,0,-1): #days loop
+        if (a_stock_info.history[i].maxprice>maxprice): 
+             days_list.append(a_stock_info.history[i].maxprice)
+             if (len(days_list)>days): del days_list[0]
+             a_stock_info.analysis[i].twoh_fifty_days_max=max(days_list)
+             
+def twoh_fifty_days_min_generation(a_stock_info):
+    days = 250
+    minprice = 100000.0;
+    days_list = list()
+    for i in range (len(a_stock_info.history)-1,0,-1): #days loop
+        if (a_stock_info.history[i].minprice<minprice): 
+             days_list.append(a_stock_info.history[i].minprice)
+             if (len(days_list)>days): del days_list[0]
+             a_stock_info.analysis[i].twoh_fifty_days_min=min(days_list)  
+        
+    
+def analysis_generation():
+    stockdata_in_days = IOModule.fetch_all_stock_data()
+    stockdata_in_stock_struct = IOModule.transfer_day_struct_2_stock_stock(stockdata_in_days)
+    for i in stockdata_in_stock_struct.keys():
+        five_days_MA_generation_for_a_stock(stockdata_in_stock_struct[i])
+        ten_days_MA_generation_for_a_stock(stockdata_in_stock_struct[i])
+        twnty_days_MA_generation_for_a_stock(stockdata_in_stock_struct[i])
+        sixty_days_MA_generation_for_a_stock(stockdata_in_stock_struct[i])
+        twoh_fifty_MA_generation_for_a_stock(stockdata_in_stock_struct[i])
+        twnty_days_max_generation(stockdata_in_stock_struct[i])
+        twnty_days_min_generation(stockdata_in_stock_struct[i])
+        sixty_days_max_generation(stockdata_in_stock_struct[i])
+        sixty_days_min_generation(stockdata_in_stock_struct[i])
+        twoh_fifty_days_max_generation(stockdata_in_stock_struct[i])
+        twoh_fifty_days_min_generation(stockdata_in_stock_struct[i])
+        
+analysis_generation()
+    
     
 
